3a07fe8fc77231c8d7307321cf603f53deeee2bc
EwerssonSllv <ewerssonsllv@gmail.com>
2026-01-02 01:47:56 -0300

    The Private class was created so that only logged-in users can access a specific route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router"
-
 import { Home } from "./pages/home"
 import { Login } from "./pages/login"
 import { Admin } from "./pages/admin"
 import { Networks } from "./pages/networks"
+import { Private } from "./routes/private"
 
 const router = createBrowserRouter([
   {
@@ -16,11 +16,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: <Admin />
+    element: <Private><Admin/></Private>
   },
   {
     path: "admin/social",
-    element: <Networks/>
+    element: <Private><Networks/></Private>
   },
   
 ])
