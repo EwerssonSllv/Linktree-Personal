1e3101788c91026e923af6aff74d2dda2dcf11d5
EwerssonSllv <ewerssonsllv@gmail.com>
2026-01-02 02:51:03 -0300

    Completed form

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import { Header } from "../../components/header";
 import { Input } from "../../components/input";
+import { FiTrash } from "react-icons/fi";
 
 
 export function Admin() {
     const [nameInput, setNameInput] = useState("")
     const [urlInput, setUrlInput] = useState("")
-    const [textColorInput, setTextColorInput] = useState("#f1f1f1")
-    const [backgroundColorInput, setBackgroundColorInput] = useState("#121212")
+    const [textColorInput, setTextColorInput] = useState("#121212")
+    const [backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1")
 
     return (
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header />
             <form className="flex flex-col mt-8 w-full max-w-xl">
-                <label className="text-white font-medium mt-2 mb-2 ">Link name</label>
+                <label className="text-white font-medium mt-2 mb-2 ">Name</label>
                 <Input placeholder="Link name"
                     value={nameInput}
                     onChange={(e) => setNameInput(e.target.value)} />
-                <label className="text-white font-medium mt-2 mb-2 ">Link URL</label>
+                <label className="text-white font-medium mt-2 mb-2 ">URL</label>
                 <Input placeholder="Link URL"
                     type="url"
                     value={urlInput}
@@ -41,15 +42,43 @@ export function Admin() {
                             onChange={(e) => setBackgroundColorInput(e.target.value)} />
                     </div>
                 </section>
-                <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
-                    <label className="text-white font-medium mt-2 mb-3">Preview</label>
-                    <article
-                        className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
-                        style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput, }}>
-                        <p className="font-medium" style={{ color: textColorInput }}>LinkedIn</p>
-                    </article>
-                </div>
+                {nameInput != "" &&
+                    <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+                        <label className="text-white font-medium mt-2 mb-3">Preview</label>
+                        <article
+                            className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
+                            style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput, }}>
+                            <p className="font-medium" style={{ color: textColorInput }}>{nameInput}</p>
+                        </article>
+                    </div>}
+
+                <button type="submit" className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center">
+                    Register
+                </button>
             </form>
+
+            <h2 className="font-bold text-white mb-4 text-2xl ">
+                My Links
+            </h2>
+
+            <article
+
+             className="flex justify-between items-center w-11/12 max-w-xl rounded py-3 px-2 mb-2"
+
+             style={{backgroundColor: "#2563EB", color: "#000"}}
+             
+             >
+                <p>Youtube</p>
+                <div>
+                    <button
+
+                    className="border border-dashed py-1 px-2 rounded border-white"
+                    
+                    >
+                        <FiTrash size={16} color="white"/>
+                    </button>
+                </div>
+            </article>
         </div>
     )
 }
